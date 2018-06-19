b0d0c001234a63455437ceaeae0ad52bd2d635fc
Zharnite <zharnite@gmail.com>
2018-06-18 22:24:27 -0400

    now we have click power

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,37 @@ function blobs(n) {
 	totalBlobsEl.innerHTML = totalBlobs;
 }
 
-powerUpCost 
+// Equation: Price=BaseCost×1.15^(#Owned)
+
+// for clicking blob
+clickPower = 1;
+
+redBlob = document.getElementById("redBlob");
+redBlob.addEventListener("click", function() {
+	blobs(clickPower);
+})
+
+// for clicking power up
 powerUpCostEl = document.getElementById("powerUpCost");
 powerUpLevelEl = document.getElementById("powerUpLevel");
 powerUpBuyOneEl = document.getElementById("powerUpBuyOne");
+
+powerUpLevel = 0;
+powerUpBuyOneEl.addEventListener("click", function() {
+	price = Math.floor(10 * Math.pow(1.1, powerUpLevel));
+	if(totalBlobs >= price) {
+		clickPower ++;
+		powerUpLevel++;
+		totalBlobs -= price;
+		powerUpLevelEl.innerHTML = powerUpLevel;
+		totalBlobsEl.innerHTML = totalBlobs;
+		powerUpCostEl = Math.floor(10 * Math.pow(1.1, powerUpLevel));
+	}
+});
+
+
+// increments
+window.setInterval(function(){
+	
+	
+}, 1000);
