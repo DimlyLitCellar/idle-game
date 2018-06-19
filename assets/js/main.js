87723dd96d11b377d0ffe1c6c96ef1d6de043711
Zharnite <zharnite@gmail.com>
2018-06-18 22:45:39 -0400

    updating the left col

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,12 @@ function blobs(n) {
 
 // Equation: Price=BaseCost×1.15^(#Owned)
 
-// for clicking blob
+// for showing click power
 clickPower = 1;
+clickPowerEl = document.getElementById("clickPower");
+clickPowerEl.innerHTML = clickPower;
 
+// for clicking blob
 redBlob = document.getElementById("redBlob");
 redBlob.addEventListener("click", function() {
 	blobs(clickPower);
@@ -28,15 +31,33 @@ powerUpBuyOneEl.addEventListener("click", function() {
 		clickPower ++;
 		powerUpLevel++;
 		totalBlobs -= price;
+		clickPowerEl.innerHTML = clickPower;
 		powerUpLevelEl.innerHTML = powerUpLevel;
 		totalBlobsEl.innerHTML = totalBlobs;
 		powerUpCostEl.innerHTML = Math.floor(10 * Math.pow(1.1, powerUpLevel));
 	}
 });
 
+//for workers
+miniBlobWorkerCostEl = document.getElementById("miniBlobWorkerCost");
+miniBlobWorkerAmountEl = document.getElementById("miniBlobWorkerAmount");
+miniBlobWorkerBuyOneEl = document.getElementById("miniBlobWorkerBuyOne");
+
+miniBlobWorkerAmount = 0;
+miniBlobWorkerBuyOneEl.addEventListener("click", function() {
+	price = Math.floor(20 * Math.pow(1.1, miniBlobWorkerAmount));
+	if(totalBlobs >= price) {
+		miniBlobWorkerAmount++;
+		totalBlobs -= price;
+		miniBlobWorkerAmountEl.innerHTML = miniBlobWorkerAmount;
+		totalBlobsEl.innerHTML = totalBlobs;
+		miniBlobWorkerCostEl.innerHTML = Math.floor(20 * Math.pow(1.1, miniBlobWorkerAmount));
+
+	}
+})
 
 // increments
 window.setInterval(function(){
-	
+	blobs(miniBlobWorkerAmount);
 	
 }, 1000);
